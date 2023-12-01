d4b4be788531f1aced37fee107a763b657f8e4f7
abhay yadav <08abhayyadav@gmail.com>
2023-12-01 23:38:08 +0530

    added till code generation

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 import * as z from "zod";
 import { Heading } from "@/components/heading";
-import { MessageSquare } from "lucide-react";
+import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { formSchema } from "./constants";
@@ -18,10 +18,10 @@ import { Loader } from "@/components/loader";
 import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
+import ReactMarkdown from "react-markdown"
 
 
-
-const ConversationPage = () => {
+const CodePage = () => {
     const router = useRouter();
 
 
@@ -49,7 +49,7 @@ const ConversationPage = () => {
 
             const newMessages = [...messages, userMessage];
 
-            const response = await axios.post("/api/conversation", {
+            const response = await axios.post("/api/code", {
                 messages: newMessages,
             });
 
@@ -71,11 +71,11 @@ const ConversationPage = () => {
     return (
         <div>
             <Heading
-                title="Conversation"
-                description="our most advanced conversation model"
-                icon={MessageSquare}
-                iconColor="text-violet-500"
-                bgColor="bg-violet-500/10"
+                title="Code Generation"
+                description="Generate code using descriptive text"
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
             />
 
             <div className="px-4 lg:px-8">
@@ -96,7 +96,7 @@ const ConversationPage = () => {
                                           focus-visible:ring-0 
                                           focus-visible:ring-transparent"
                                                 disabled={isLoading}
-                                                placeholder="how do i calculate the radius of a circle"
+                                                placeholder="simple counter using useState hook"
                                                 {...field}
                                             />
 
@@ -135,7 +135,22 @@ const ConversationPage = () => {
                                         message.role === "user" ? "bg-white border border-black/10" : "bg-muted")}
                                 >
                                     {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                                    <p className="text-sm">{message.content}</p>
+                                    <ReactMarkdown
+                                        components={{
+                                            pre: ({ node, ...props }) => (
+                                                <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                                    <pre {...props} />
+                                                </div>
+                                            ),
+                                            code: ({ node, ...props }) => (
+                                                <code className="bg-black/10 rouded-lg p-1" {...props} />
+                                            )
+                                        }}
+
+                                        className="text-sm overflow-hidden leading-7"
+                                    >
+                                        {message.content || ""}
+                                    </ReactMarkdown>
                                 </div>
                             ))
                         }
@@ -145,4 +160,4 @@ const ConversationPage = () => {
         </div>
     );
 }
-export default ConversationPage;
+export default CodePage;
